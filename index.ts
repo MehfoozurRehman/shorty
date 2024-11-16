65bd52d17c73373d8d0811678ce27ea07e6463bd
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2024-11-16 13:37:48 +0500

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,110 @@
+import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client";
+import { logger } from "hono/logger";
+import { serve } from "@hono/node-server";
+
+const prisma = new PrismaClient();
+
+const app = new Hono().basePath("/api");
+
+app.use(logger());
+
+const checkShortUrl = async (shortUrl: string) => {
+  const url = await prisma.url.findUnique({
+    where: { shortUrl },
+    select: { id: true },
+  });
+
+  return !!url;
+};
+
+async function main() {
+  app.get("/", (c) => {
+    return c.json({ message: "Hello World" });
+  });
+
+  app.get("/:shortUrl", async (c) => {
+    try {
+      const { shortUrl } = c.req.param();
+
+      const url = await prisma.url.findUnique({
+        where: { shortUrl },
+        select: { url: true },
+      });
+
+      if (!url) {
+        return c.json({ message: "Not Found" }, 404);
+      }
+
+      await prisma.url.update({
+        where: { shortUrl },
+        data: { clicks: { increment: 1 } },
+      });
+
+      return c.redirect(url.url);
+    } catch (e) {
+      console.error(e);
+      return c.json({ message: "Internal Server Error" }, 500);
+    }
+  });
+
+  app.post("/", async (c) => {
+    try {
+      const { url: queryUrl } = c.req.query();
+      const { url: bodyUrl } = await c.req.json();
+
+      const url = queryUrl || bodyUrl;
+
+      if (!url) {
+        return c.json({ message: "Bad Request" }, 400);
+      }
+
+      let shortUrl = "";
+      const maxAttempts = 10;
+
+      for (let count = 0; count < maxAttempts; count++) {
+        shortUrl =
+          Math.random().toString(36).substring(2, 8) + Date.now().toString(36);
+        if (!(await checkShortUrl(shortUrl))) {
+          break;
+        }
+        if (count === maxAttempts - 1) {
+          return c.json(
+            { message: "Could not generate unique short URL" },
+            500
+          );
+        }
+      }
+
+      await prisma.url.create({
+        data: {
+          url,
+          shortUrl,
+        },
+      });
+
+      return c.json({ shortUrl });
+    } catch (e) {
+      console.error(e);
+      return c.json({ message: "Internal Server Error" }, 500);
+    }
+  });
+}
+
+main()
+  .then(() => {
+    console.log("Server is ready");
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
+
+const port = 3000;
+console.log(`Server is running on http://localhost:${port}`);
+
+serve({
+  fetch: app.fetch,
+  port,
+});
